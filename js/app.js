c107abbed0fa5d707fc17190a40eeb6a90cb2551
howardop <hlo4@cornell.edu>
2018-07-03 12:41:14 -0400

    Create new shuffled deck

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
-
+let deck = ["fa-diamond", 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',                           'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt',
+                     'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf',
+                     'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb']
 
 /*
  * Display the cards on the page
@@ -9,6 +11,25 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+let template =  '<li class="card">' + 
+'<i class="fa fa-bomb"></i>' + 
+'</li>';
+
+function createNewDeck() {
+    shuffle(deck);
+
+    // Now create HTML for each card in deck, and add it to the new deck.
+    let newDeck = '';
+    deck.forEach((card) => {
+        let newCard = `<li class="card"> <i class="fa ${card}"></i> </li>`;
+        console.log(newCard);
+        newDeck += newCard;
+    });
+
+    // Put the deck just created into the proper spot in the DOM
+    let ulDeck = document.querySelector('ul.deck');
+    ulDeck.innerHTML=newDeck;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,6 +46,9 @@ function shuffle(array) {
 
     return array;
 }
+
+
+createNewDeck();
 
 
 /*
@@ -45,53 +69,64 @@ let cards = document.querySelectorAll(".card");
 // Card flipping
 //  'open' class changes card's background color
 //  'show' class causes icon to be displayed
-cards.forEach(function(card){
-    card.addEventListener('click', function(e) {
-        if (openCards.length === 0) {
-            card.classList.add('open','show');
-            openCards.push(card);
-        }  else if (openCards.length === 1) {
-            card.classList.add('open','show');
-            openCards.push(card);
-            
-            // 2 cards now open.  Close them after 5 seconds
-            setTimeout(function (){
-                openCards.forEach(function(card){
-                    card.classList.remove('open','show');
-                });// end openCards.forEach
-                openCards.splice(0,2);
-            }, // End timeout function
-            5000); // end setTimeout
+cards.forEach(function (card) {
+    card.addEventListener('click', function (e) {
+        // First check if card has already been clicked on during this turn or already matched
+        if ((card.classList.contains('open') && card.classList.contains('show')) ||
+            card.classList.contains('match')) {
+            // Card is already exposed.  Return
+            return;
         }
 
-    });// end addEventListener 'click'
+        if (openCards.length === 0) {
+            card.classList.add('open', 'show');
+            openCards.push(card);
+        } else if (openCards.length === 1) {
+            card.classList.add('open', 'show');
+            openCards.push(card);
+
+            // Now need to check for match
+            let image1 = getImage(openCards[0].querySelector('i'));
+            console.log('image1 = ' + image1);
+            let image2 = getImage(openCards[1].querySelector('i'));
+            console.log('image2 = ' + image2);
+            if (image1 == image2) {
+                // Match!
+                openCards[0].classList.add('match');
+                openCards[1].classList.add('match');
+                openCards.splice(0, 2);
+            } else {
+
+                // Cards don't match.  Close them after 5 seconds
+                setTimeout(function () {
+                        openCards.forEach(function (card) {
+                            card.classList.remove('open', 'show');
+                        }); // end openCards.forEach
+                        openCards.splice(0, 2);
+                    }, // End timeout function
+                    1000); // end setTimeout
+            }
+        }
+
+    }); // end addEventListener 'click'
 })
 
-function clickHandler(event) {
-    let card = event.target;
-    card.classList.add('open', 'show');
-    if (openCards.length == 0) {
-        openCards.push(card);
-        console.log('card.classList = ' + card.classList);
-    } else if (openCards.length == 1) {
-        openCards.push(card);
-        // 2 cards now selected.  Check for match
-
-        console.log('Waiting 5 seconds now');
-        // Wait 5 seconds and then hide cards
-        setTimeout(revertCards(), 5000);
-
+// getImage gets the class name for the image in the card
+function getImage(icon) {
+    let classes = icon.classList;
+    //Search classes array for class that begins with 'fa-' that is the image name
+    for (let i = 0; i < classes.length; i++) {
+        if (classes[i].search('fa-') >= 0) {
+            return classes[i]
+        }
     }
-
+    return null;
 }
 
-function revertCards() {
-    console.log('Inside revertCards');
-    console.log('openCards = ' + openCards);
-    openCards.forEach((card) => {
-        card.classList.remove('open', 'show');
-        //card.classList.remove('show');
-    });
-    openCards.splice(0, 2);
-    console.log('openCards.length = ' + openCards.length);
-}
+// Reset game
+let reset = document.querySelector('.restart');
+reset.addEventListener('click', function() {
+    openCards = [];
+    cards = null;
+    createNewDeck();
+})
